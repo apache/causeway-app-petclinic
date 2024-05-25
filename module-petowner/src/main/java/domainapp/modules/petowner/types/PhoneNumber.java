80554c1932f34bae2963a5d4f4eed1c2e97ace0b
Dan Haywood <dan@haywood-associates.co.uk>
2024-05-25 15:25:18 +0100

    Updates PhoneNumber meta-annotation, adds regex validation

--- module-petowner/src/main/java/domainapp/modules/petowner/types/PhoneNumber.java
+++ module-petowner/src/main/java/domainapp/modules/petowner/types/PhoneNumber.java
@@ -14,15 +14,24 @@ import org.apache.causeway.applib.annotation.Property;
 @Property(
         editing = Editing.ENABLED,
         maxLength = PhoneNumber.MAX_LEN,
-        optionality = Optionality.OPTIONAL
+        optionality = Optionality.OPTIONAL,
+        regexPattern = PhoneNumber.REGEX_PATTERN,
+        regexPatternReplacement = PhoneNumber.REGEX_PATTERN_REPLACEMENT
 )
 @Parameter(
         maxLength = PhoneNumber.MAX_LEN,
-        optionality = Optionality.OPTIONAL
+        optionality = Optionality.OPTIONAL,
+        regexPattern = PhoneNumber.REGEX_PATTERN,
+        regexPatternReplacement = PhoneNumber.REGEX_PATTERN_REPLACEMENT
 )
 @Target({ ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PhoneNumber {
 
     int MAX_LEN = 40;
+    String REGEX_PATTERN = "[+]?[0-9 ]+";
+    String REGEX_PATTERN_REPLACEMENT =
+            "Specify only numbers and spaces, optionally prefixed with '+'.  " +
+            "For example, '+353 1 555 1234', or '07123 456789'";
+
 }
