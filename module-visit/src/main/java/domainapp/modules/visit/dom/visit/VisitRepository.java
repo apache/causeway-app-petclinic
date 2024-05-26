6e5b7f449947516ae71b1476feb43eb163d5a003
Dan Haywood <dan@haywood-associates.co.uk>
2024-05-26 23:32:23 +0100

    updates VisitRepository, adds findByVisitAfter

--- module-visit/src/main/java/domainapp/modules/visit/dom/visit/VisitRepository.java
+++ module-visit/src/main/java/domainapp/modules/visit/dom/visit/VisitRepository.java
@@ -1,5 +1,6 @@
 package domainapp.modules.visit.dom.visit;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +9,8 @@ import org.springframework.data.repository.Repository;
 import domainapp.modules.petowner.dom.petowner.PetOwner;
 
 public interface VisitRepository extends Repository<Visit, Integer> {
+
+    List<Visit> findByVisitAtAfter(LocalDateTime visitAt);
 
     @Query("select v from Visit v where v.pet.petOwner = :petOwner")
     List<Visit> findByPetOwner(PetOwner petOwner);
