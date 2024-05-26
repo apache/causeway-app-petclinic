6bc918fd58d3a4347134482778f2142a09aa7282
Dan Haywood <dan@haywood-associates.co.uk>
2024-05-26 22:32:08 +0100

    adds a subscriber to veto deletion if there are any visits for the owner

---
+++ module-visit/src/main/java/domainapp/modules/visit/subscriptions/PetOwner_delete_subscriber.java
@@ -0,0 +1,39 @@
+package domainapp.modules.visit.subscriptions;
+
+import domainapp.modules.petowner.dom.petowner.PetOwner;
+import domainapp.modules.visit.dom.visit.Visit;
+import domainapp.modules.visit.dom.visit.VisitRepository;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PetOwner_delete_subscriber {
+
+    @EventListener(PetOwner.DeleteActionDomainEvent.class)
+    void on(PetOwner.DeleteActionDomainEvent event) {
+        PetOwner subject = event.getSubject();
+        switch (event.getEventPhase()) {
+            case HIDE:
+                break;
+            case DISABLE:
+                List<Visit> visits = visitRepository.findByPetOwner(subject);
+                if (!visits.isEmpty()) {
+                    event.veto("This owner has %d visit%s", visits.size(), (visits.size() == 1 ? "" : "s"));
+                }
+                break;
+            case VALIDATE:
+                break;
+            case EXECUTING:
+                break;
+            case EXECUTED:
+                break;
+        }
+    }
+
+    @Inject VisitRepository visitRepository;
+}
