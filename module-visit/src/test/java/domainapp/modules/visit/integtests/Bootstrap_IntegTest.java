687028632de67960ce6614b095769b8748c71a1b
Dan Haywood <dan@haywood-associates.co.uk>
2024-05-26 23:14:52 +0100

    adds Bootstrap_IntegTest to check setup

---
+++ module-visit/src/test/java/domainapp/modules/visit/integtests/Bootstrap_IntegTest.java
@@ -0,0 +1,9 @@
+package domainapp.modules.visit.integtests;
+
+import org.junit.jupiter.api.Test;
+
+public class Bootstrap_IntegTest extends VisitModuleIntegTestAbstract {
+
+    @Test
+    public void checks_can_bootstrap() {}
+}
