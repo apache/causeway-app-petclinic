250f836b8853c5e252a41738644d0ead764b0f17
Dan Haywood <dan@haywood-associates.co.uk>
2024-06-03 13:26:05 +0100

    updates javax to jakarta

--- module-visit/src/main/java/domainapp/modules/visit/contributions/PetOwner_visits.java
+++ module-visit/src/main/java/domainapp/modules/visit/contributions/PetOwner_visits.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 
 import org.apache.causeway.applib.annotation.Action;
 import org.apache.causeway.applib.annotation.Collection;
