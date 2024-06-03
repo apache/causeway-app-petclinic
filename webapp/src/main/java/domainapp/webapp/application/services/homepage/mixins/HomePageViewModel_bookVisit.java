5702ed6c89ef978a784c472942b743ccebfd5563
Dan Haywood <dan@haywood-associates.co.uk>
2024-06-03 13:27:02 +0100

    updates javax to jakarta

--- webapp/src/main/java/domainapp/webapp/application/services/homepage/mixins/HomePageViewModel_bookVisit.java
+++ webapp/src/main/java/domainapp/webapp/application/services/homepage/mixins/HomePageViewModel_bookVisit.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 
 import org.apache.causeway.applib.annotation.Action;
 import org.apache.causeway.applib.annotation.ActionLayout;
