6406bf5904450e36a0aba3f21e0bca4b4f7351b0
Dan Haywood <dan@haywood-associates.co.uk>
2024-06-03 14:20:29 +0100

    updates javax to jakarta

--- module-visit/src/main/java/domainapp/modules/visit/fixture/Visit_persona.java
+++ module-visit/src/main/java/domainapp/modules/visit/fixture/Visit_persona.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 
 import org.apache.causeway.applib.services.wrapper.control.SyncControl;
 
